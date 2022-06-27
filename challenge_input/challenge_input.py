df7bcf36e7eceb0ed95be4f07b60f815b21d3556
Patrick Haggerty <Hagpoh@users.noreply.github.com>
2022-06-27 22:09:13 +0000

    All day 1 labs and challenges for Python and Linux

---
+++ challenge_input/challenge_input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+
+    name_input = input("Please enter your name:  ")
+    day_input = input("Please enter the day of week: ")
+     
+    print("Hello", name_input + "!", "Happy " + day_input + "!")
+    
+main()
+
